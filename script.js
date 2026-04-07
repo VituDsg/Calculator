cfb5d3c951a55f5cedd2e7068a5ad8119d7650e4
Vitudsg <vtvitucosta@gmail.com>
2026-04-07 16:36:05 -0300

    Finalizing calculator in HTML and CSS

---
+++ script.js
@@ -0,0 +1,27 @@
+function add (a, b) {
+    return a + b;
+
+}
+
+function subtract (a, b) {
+    return a - b;
+
+}
+
+function multiply (a, b) {
+    return a * b;
+
+}
+
+function divide (a, b) {
+    return a/b;
+}
+
+function operate (op, a, b) {
+    
+}
+
+console.log(add(5, 6))
+console.log(subtract(5,3))
+console.log(multiply(2,5))
+console.log(divide(10,2))
